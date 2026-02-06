e274550b99f25434cd3ffbb48e287157e8ba11c7
josefainostroza <62558856+josefainostroza@users.noreply.github.com>
2026-02-06 07:43:11 -0300

    Add files via upload

---
+++ project-details.js
@@ -0,0 +1,96 @@
+export const PROJECT_DETAILS = {
+	uxui: {
+		projects: [
+			{
+				slug: 'trailbi',
+				img: '/assets/TRAILBIPROYECTOYES.png',
+				video: '/assets/videos/Testeotrailvi.gif'
+			},
+			{
+				slug: 'emigra-facil',
+				title: 'EMIGRAFÁCIL',
+				img: '/assets/Emigrafacilproyecto.png'
+			},
+			{
+				slug: 'bumu',
+				img: '/assets/bumucompleto.svg',
+				img02: '/assets/Designsystembumu.svg'
+			},
+			{
+				slug: 'Mcflurry-KitKat',
+				img: '/assets/TechoProyectoo.svg'
+			}
+		]
+	},
+	desarrollo: {
+		projects: [
+			{
+				slug: 'space-tourism-website',
+				img: '/assets/space-tourism-website.png'
+			},
+			{
+				slug: 'product-list-cart',
+				title: 'EMIGRAFÁCIL',
+				img: '/assets/Product-list.png'
+			},
+			{
+				slug: 'interactive-card-form',
+				img: '/assets/bumucompleto.svg',
+				img02: '/assets/Interactive-card.png'
+			},
+			{
+				slug: 'sunnyside-agency',
+				img: '/assets/Sunnyside-agency.png'
+			},
+			{
+				slug: 'todo-app',
+				img: '/assets/todo-app.jpg'
+			}
+		]
+	},
+	interaccion: {
+		projects: [
+			{
+				slug: 'conqui-planet',
+				img: '/assets/laminafinalconqui.jpg'
+			},
+			{
+				slug: 'lugares-valdivia',
+				title: 'VISUALIZACIÓN DE LUGARES EN VALDIVIA Y SU ACCESIBILIDAD',
+				subtitle:
+					'Desafío del proyecto Fabric de Siemens, trabajando con data de la ciudad para visualizarla a través de piezas interactivas. Los resultados del encargo no son visualizaciones para informar objetivamente, sino más bien interpretan la data para transmitir una idea, insight o conclusiones propias.',
+				img: '/assets/LugaresDeValdivia.png',
+				info: 'El desafío del proyecto Fabric de Siemens era cruzar e interpretar data de fuentes oficiales del Estado/Gobierno y visualizarla a través de piezas interactivas. Los resultados del encargo no son visualizaciones para informar objetivamente, sino más bien interpretan la data para transmitir una idea, insight o conclusiones propias. Esta visualización interactiva se realizó a partir de datos de niveles de accesibilidad en 97 lugares de acceso público en la ciudad de Valdivia, evaluando accesibilidad exterior, accesibilidad interior, y accesibilidad a sus baños. A través de la interacción con teclado, se pudo explorar la data y visualizar, por ejemplo, los recintos que tienen mejor o peor calificación en todas las categorías, o cada una por separado; lo que permitió evidenciar visualmente el diagnóstico realizado y relevar la importancia del cumplimiento de la normativa vigente sobre accesibilidad a recintos públicos.',
+				Asignature: 'Asignatura: Programación creativa | Diseño UDD',
+				teacher: 'Profesor Guía: Francisco Zamorano',
+				team: '',
+				año: 'Año: 2021',
+				cliente: 'Cliente: Fabric de Siemens',
+				rol: 'Rol: Diseñadora gráfica, Desarrolladora',
+				program01: '/assets/programs/Processing.png',
+				program02: '/assets/programs/AI.png',
+				video: '/assets/videos/Videovaldivia.gif'
+			}
+		]
+	},
+	grafico: {
+		projects: [
+			{
+				slug: 'redes-sociales',
+				img: '/assets/RLANDING.jpg'
+			},
+			{
+				slug: 'branding',
+				img: '/assets/LANDINDINDIANOMADE.png'
+			},
+			{
+				slug: 'presidiaria',
+				img: '/assets/LandingPresidiaria.png'
+			},
+			{
+				slug: 'fotografia-photoshop',
+				img: '/assets/LANDINGFOTOGRAFIA.png'
+			}
+		]
+	}
+};
---
+++ title-proyects.js
@@ -0,0 +1,127 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export const TITLE_PROYECTS01 = [
+	{
+		id: uuidv4(),
+		title: 'DISEÑO UX/UI',
+		projects: [
+			{
+				name: 'Trailbi',
+				image: '/assets/Trailbi.png',
+				slug: 'trailbi',
+				url: 'https://www.figma.com/proto/W2rqbWEH6CemH5Ogh4zSLE/Trailbi?node-id=6-57&t=JsGaWno46bE26TsO-0&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1'
+			},
+			{
+				name: 'Emigra Fácil',
+				image: '/assets/EmigraFacil.png',
+				slug: 'emigra-facil',
+				url: 'https://www.figma.com/proto/qy5ME2MbKusqOt9gaJaMCM/Emigra-F%C3%A1cil?node-id=1-44&t=sT57CsUboDRiCHJA-1&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1'
+			},
+			{
+				name: 'BuMu',
+				image: '/assets/BUMU.png',
+				slug: 'bumu',
+				url: 'https://www.figma.com/proto/lDtmVFqyoXua3H8qWjuARk/App-Museo?node-id=635-7050&t=W0k5iUSevV1GZ9WT-1&scaling=scale-down-width&content-scaling=fixed&page-id=422%3A7063&starting-point-node-id=635%3A7050'
+			},
+			{
+				name: 'Mcflurry-KitKat',
+				image: '/assets/TechoParaChile.png',
+				slug: 'techo',
+				url: 'https://promos.mcdonalds.es/mcflurry/kitkat/'
+			}
+		]
+	},
+	{
+		id: uuidv4(),
+		title: 'DESARROLLO WEB FRONT-END',
+		projects: [
+			{
+				name: 'Space Tourism Website',
+				image: '/assets/space-tourism-website.png',
+				slug: 'space-tourism-website',
+				url: 'https://github.com/josefainostroza/portfolio'
+			},
+			{
+				name: 'Product List with Cart',
+				image: '/assets/Product-list.png',
+				slug: 'product-list-cart',
+				url: 'https://github.com/josefainostroza/portfolio'
+			},
+			{
+				name: 'Interactive Card Form',
+				image: '/assets/Interactive-card.png',
+				slug: 'interactive-card-form',
+				url: 'https://github.com/josefainostroza/portfolio'
+			},
+			{
+				name: 'Sunnyside Agency Landing',
+				image: '/assets/Sunnyside-agency.png',
+				slug: 'sunnyside-agency',
+				url: 'https://github.com/josefainostroza/portfolio'
+			},
+			{
+				name: 'Todo App',
+				image: '/assets/todo-app.jpg',
+				slug: 'todo-app',
+				url: 'https://github.com/josefainostroza/portfolio'
+			}
+		]
+	}
+];
+
+export const TITLE_PROYECTS02 = [
+	{
+		id: uuidv4(),
+		title: 'DISEÑO DE INTERACCIÓN',
+		projects: [
+			{
+				name: 'Conqui Planet',
+				image: '/assets/ConquiPlanet.png',
+				slug: 'conqui-planet',
+				url: 'https://www.figma.com/proto/wXhMr1tSSsCmQefbxPxA2U/Conqui-Planet?node-id=1-7&t=JsGaWno46bE26TsO-0&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1'
+			},
+			{
+				name: 'Valdivia y su Accesibilidad',
+				image: '/assets/LugaresDeValdivia.png',
+				slug: 'lugares-valdivia',
+				url: 'https://www.figma.com/proto/7uGO1mbA1hSSGfrpKB5L3v/Lugares-de-valdivia-y-su-accesibilidad?node-id=1-2&t=YMI5Wb24hvWYrLSe-1&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1'
+			},
+			{
+				name: 'Auracuentos',
+				image: '/assets/Taller de Objetos Inteligentes.png',
+				slug: 'aura-cuentos',
+				url: 'https://www.figma.com/proto/PY1UEXS1LXZCTDgFHbiK1G/Untitled?node-id=3-2&p=f&t=HO4BGmqYCr3BwrJl-1&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1'
+			}
+		]
+	},
+	{
+		id: uuidv4(),
+		title: 'DISEÑO GRÁFICO',
+		projects: [
+			{
+				name: 'Redes Sociales',
+				image: '/assets/RedesSociales.jpg',
+				slug: 'redes-sociales',
+				url: 'https://www.figma.com/proto/jVevA4xYCZ5uS6Vt6j3xic/Untitled?node-id=1-2&t=vdYebQJYK4D90mgV-1&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1'
+			},
+			{
+				name: 'Branding',
+				image: '/assets/logobranding.png',
+				slug: 'branding',
+				url: 'https://www.figma.com/proto/04shrzYgcEXXt5lsG8VYvM/Branding?node-id=2-2&p=f&t=Ohl6itLrGehCKJCf-1&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1'
+			},
+			{
+				name: 'Presidiaria',
+				image: '/assets/portadaletra.png',
+				slug: 'presidiaria',
+				url: 'https://www.figma.com/proto/HtPrUbqWJv5aOxX8sK64i1/Presidiaria?node-id=1-2&t=w4Ff8cAJMPZbM24A-1&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1'
+			},
+			{
+				name: 'Fotografía y Photoshop',
+				image: '/assets/Fotografia.jpg',
+				slug: 'fotografia-photoshop',
+				url: 'https://www.figma.com/proto/H8HWybSz3nsRqBD9OMsCVl/Untitled?node-id=6-281&t=tAcEleloTIjsYoW1-1&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1'
+			}
+		]
+	}
+];
